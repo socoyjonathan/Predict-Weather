e54aa1442d26dfa48b20f73d308514d50f4c1ccc
Jonathan Socoy <72121903+socoyjonathan@users.noreply.github.com>
2022-07-29 12:09:15 -0500

    Add files via upload

---
+++ predict_weather_wttr.py
@@ -0,0 +1,22 @@
+pip install requests
+
+import requests
+
+def predict_weather():
+  city = input('Please enter a city: ')
+  url = (f'https://wttr.in/{city}')
+  try:
+    data = requests.get(url)
+    T = data.text
+  except:
+    T = "Error Occurred"
+  print(T)
+
+def main():
+  print("\t\tWelcome to this Weather Forecaster!\n\n")
+  print("Just enter the city you want the weather report for and click Enter!\n\n")
+
+  predict_weather()
+
+if __name__ == "__main__":
+  main()
